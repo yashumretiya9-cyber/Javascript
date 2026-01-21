eace31a6db59f2f99f65fb82450f27c8ded8f308
yashumretiya9-cyber <yashumretiya9@gmail.com>
2026-01-21 14:35:11 +0530

    first commit

---
+++ operator.js
@@ -0,0 +1,49 @@
+// operators
+// Arithmetic, comparison, logical, assignment, unary, ternary
+
+// Arithmetic:
+// +, -, /, *, **, %
+
+// + --> Add and concatenation
+// ex. 1+2, "user" + "name"
+
+// -
+// ex. 2 - 1
+
+// /, *
+// ex. 240/2, 25*2
+
+// % (modulas) (sheshfal)
+// ex. 12%4 --> 0
+// ex. 9%2 --> 1
+
+// ** (square)(exposination)
+// ex. 2**3 --> 8
+
+
+// Comparison (ans: true, false)
+
+// ==
+// ex. 12 == 13, 12 == "12" (don't check type, only check value, not strict comparison)
+
+// ===
+// ex. 12 === "12" (check type and value, strict comparaison)
+
+// !=
+// ex. 12 != 13 (reverse of ==, not strict comparison)
+
+// !==
+// ex. 12 !== "12" (reverse of ===)
+
+// >=
+// ex. 22 >= 22
+
+// <=
+// ex. 13 <= 13
+
+// <
+// ex. 12 < 113, 12 < 9, 22 < 22
+
+// >
+// ex. 12 > 113, 12 > 9, 13 > 13
+
