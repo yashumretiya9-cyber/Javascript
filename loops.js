6b99176eec9c63d2f30cdcb876a5ee7a63411532
yashumretiya9-cyber <yashumretiya9@gmail.com>
2026-01-23 13:52:59 +0530

    first commit

---
+++ loops.js
@@ -0,0 +1,102 @@
+// loops  --  repeating code blocks
+// 1 1 1 1 1 1
+// 1 2 3 4 5 6
+
+
+
+// for loop
+// kya thi javanu chhe --> kya sudhi javany chhe --> ketli vaar javanu chhe
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+// for(let i=20; i>=10; i--){
+//     console.log(i);
+// }
+
+
+// while loop 
+// kaya thi javanu chhe --> Kaya rokavanu chhe --> Kevi rite javanu chhe
+// while (ex. 1 -> Hello world -> stop when condition false)
+// start
+// while(end){
+// code
+// change
+// }
+
+// let j = 10;
+// while(j<=20){
+//     console.log("j:", j);
+//     j++;
+// }
+
+// let a = 1;
+// while(a>=20){
+//     console.log("a:", a);
+//     a--;
+// }
+
+
+// loop
+// let k = 50;
+// while (k >= 20) {
+//     console.log(k);
+//     k++;
+// }
+
+// let a = 60;
+// while (k >= 20) {
+//     console.log(a);
+//     a--;
+// }
+
+// let c = 30;
+// while (c >= 25) {
+//     console.log(c);
+//     a--;
+// }
+// do while loop
+
+
+
+// let d="hello world";
+// while(d <= 10)      {   
+//     console.log(d);
+//     d++;
+// }
+
+
+
+// let f=50;
+// while(f >= 60)      {
+//     console.log(f);
+//     f++;
+// }
+
+
+// let q=50;
+// while(q <= 60)      {
+//     console.log(q);
+//     q++;
+// }
+
+
+
+//do while loop
+// do {} while (end);
+// start
+// do{
+// code
+// change}
+// while(end);
+// let g = 10;
+// do {
+//     console.log("do while:", g);
+//     g++;
+// } while (g <= 20);
+
+
+// let h = 10;
+// do {
+//     console.log("do while:", h);
+//     h--;
+// } while (h >= 1);
